cff2780e77d63399d0c8c0dd5cb36587cbcf6442
BogdanCC <bogdan.e.corcoveanu@gmail.com>
2018-06-04 00:56:57 +0300

    add custom logo, image for more screen densities

--- app/src/main/java/com/example/android/spacenews/Utils.java
+++ app/src/main/java/com/example/android/spacenews/Utils.java
@@ -5,10 +5,19 @@ import java.util.Locale;
 public final class Utils {
     private Utils() {
     }
+    /** Method to remove a part of a string from a certain point
+     * @param  removeString  removes the string starting at that point
+     * @return output[0]     split() splits the string in 2,
+     *                       we return the first part of the string
+     * */
     public static String removeSubStringAt(String mainString, String removeString) {
         String[] output = mainString.split(removeString);
         return output[0];
     }
+    /** Method to calculate the read time of an article given the number of words in that article
+     * We assume the reader reads 2 words per second
+     * @param wordCount - the number of words
+     * @return          a formatted String that shows the read time in minutes */
     public static String calculateReadTime(int wordCount){
         int readTimeMilliSeconds = (wordCount/2) * 1000;
         int minutes = readTimeMilliSeconds / (60 * 1000);
